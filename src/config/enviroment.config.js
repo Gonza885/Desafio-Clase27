6809b90abb80f1becfbcfc5ef7c46928f3d57f73
Gonza885 <gonza_g8l@hotmail.com>
2023-10-04 16:34:13 -0300

    correccion

--- src/config/enviroment.config.js
+++ src/config/enviroment.config.js
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("TWILIO_ACCOUNT_SID:", process.env.TWILIO_ACCOUNT_SID);
-console.log("TWILIO_AUTH_TOKEN:", process.env.TWILIO_AUTH_TOKEN);
-
 export default {
   MONGO_URL: process.env.MONGO_URL,
   MONGO_SESSION_SECRET: process.env.MONGO_SESSION_SECRET,
